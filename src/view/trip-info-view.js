692fe6fc99f6856130719f6ee5e899010cf3e7f6
sir-stefano <stepstaf@gmail.com>
2024-10-02 12:29:54 +0200

    Убирает лишние табы в разметке

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -3,16 +3,16 @@ import { createElement } from '../render.js';
 function createTripInfoTemplate() {
   return (
     `<section class="trip-main__trip-info  trip-info">
-            <div class="trip-info__main">
-              <h1 class="trip-info__title">Amsterdam — Chamonix — Geneva</h1>
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">Amsterdam — Chamonix — Geneva</h1>
 
-              <p class="trip-info__dates">18&nbsp;—&nbsp;20 Mar</p>
-            </div>
+        <p class="trip-info__dates">18&nbsp;—&nbsp;20 Mar</p>
+      </div>
 
-            <p class="trip-info__cost">
-              Total: €&nbsp;<span class="trip-info__cost-value">1230</span>
-            </p>
-          </section>`
+      <p class="trip-info__cost">
+        Total: €&nbsp;<span class="trip-info__cost-value">1230</span>
+      </p>
+    </section>`
   );
 }
 
