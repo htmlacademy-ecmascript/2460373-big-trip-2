085d950b3551bab57f446cd758c12edafb3bb1dc
sir-stefano <stepstaf@gmail.com>
2024-10-09 17:25:36 +0200

    Обучает презентер работать с моделью

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -2,23 +2,26 @@ import EventEditFormView from '../view/event-edit-form-view.js';
 import EventView from '../view/event-view.js';
 import ListView from '../view/list-view.js';
 import SortView from '../view/sort-view.js';
-import {render} from '../render.js';
+import { render } from '../render.js';
 
 export default class TripsPresenter {
 
   listComponent = new ListView();
 
-  constructor({listContainer}) {
+  constructor({ listContainer, eventsModel }) {
     this.listContainer = listContainer;
+    this.eventsModel = eventsModel;
   }
 
   init() {
+    this.listEvents = [...this.eventsModel.getEvents()];
+
     render(new SortView(), this.listContainer);
     render(this.listComponent, this.listContainer);
     render(new EventEditFormView(), this.listComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new EventView(), this.listComponent.getElement());
+    for (let i = 0; i < this.listEvents.length; i++) {
+      render(new EventView({ event: this.listEvents[i] }), this.listComponent.getElement());
     }
   }
 }
