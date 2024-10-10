53b6892cb1437eb3811c28f33210d5ae6d7ce56a
sir-stefano <stepstaf@gmail.com>
2024-10-10 20:38:14 +0200

    Обучает event-view работать с данными из модели

--- src/util.js
+++ src/util.js
@@ -1,3 +1,16 @@
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+
+const DATE_FORMATS = {
+  SHORT_DATE: 'MMM DD',
+  FULL_DATE: 'YYYY-MM-DD',
+  FULL_DATE_TIME: 'YYYY-MM-DD[T]HH:mm',
+  TIME: 'HH:mm',
+  MINUTES: 'mm[M]'
+};
+
+dayjs.extend(duration);
+
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -23,4 +36,18 @@ const createRandomIdGeneratorFromRange = (min, max) => {
   };
 };
 
-export { getRandomInteger, getRandomArrayElement, createRandomIdGeneratorFromRange };
+const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+const humanizeDate = (date, format) => date ? dayjs(date).format(format) : '';
+
+const addLeadingZero = (number) => String(number).padStart(2, '0');
+
+const getDateDifference = (earlierDate, laterDate) => {
+  let minutes = dayjs(laterDate).diff(dayjs(earlierDate), 'minute');
+  const hours = Math.floor(minutes / 60);
+  minutes = minutes - (hours * 60);
+  const difference = `${addLeadingZero(hours)}H ${addLeadingZero(minutes)}M`;
+  return difference;
+};
+
+export { getRandomInteger, getRandomArrayElement, createRandomIdGeneratorFromRange, capitalizeFirstLetter, humanizeDate, DATE_FORMATS, getDateDifference };
